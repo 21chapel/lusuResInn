ef2213f589cab513a2f6a742f7ce6081bd7fd08f
Anoop Anthony <anoop.anthony@gmail.com>
2019-06-25 10:29:57 -0230

    logic

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { TbayComponent } from './tbay/tbay.component';
+import { OrilliaComponent } from './orillia/orillia.component';
+import { EventDetailComponent } from './event-detail/event-detail.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'thunderbay', component: TbayComponent},
+  { path: 'thunderbay/:id', component: EventDetailComponent},
+  { path: 'orillia', component: OrilliaComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,23 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { OrilliaComponent } from './orillia/orillia.component';
+import { TbayComponent } from './tbay/tbay.component';
+import { EventDetailComponent } from './event-detail/event-detail.component';
+import { EventService } from './event.service';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    OrilliaComponent,
+    TbayComponent,
+    EventDetailComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule
   ],
-  providers: [],
+  providers: [ EventService ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
